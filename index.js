d4b7e05beb2e2fa0e9433b341fdd369dc8af691b
Eze Onyebuchi <eze4acme@gmail.com>
2023-07-10 12:57:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight)
     createElement('h1', 'love p5js')
+    print('hello')
 }
 
 function draw() {
     background(100, 0, 200);
     rectMode(CENTER);
+    strokeWeight(60)
+    fill(100, 100, 100, 125)
     rect(100, 100, 100, 100, 50);
     // triangle(30, 75, 58, 20, 86, 75);
 }
