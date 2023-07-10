3f440fd27a68f26aead5c5b00dd97366c04a968a
Eze Onyebuchi <eze4acme@gmail.com>
2023-07-10 14:42:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
 
-
-
-let circleX = 100
+// let circleX = 100
  function setup() {
     createCanvas(600, 600)
+    background(0)
     // createElement('h1', 'love p5js')
     // print('hello')
 }
-function mousePressed() {
-    circleX = 0
-}
+// function mousePressed() {
+//     circleX = 0
+// }
 
+// function draw() {
+//     background(0);
+//     noStroke();
+//     fill(255)
+//     random(300, 300)
+//     rect(circleX, 200, 100, 200)
+//     circleX += 5
+// }
+
+// let squareSize;
+// let lineWidth ;
+
+// function draw() {
+//     background(100);
+//     rectMode(CENTER);
+//     strokeWeight(random(50, 100))
+//     stroke(0, 0, 255)
+//     fill(0,255,0)
+//     square(200, 150, random(200, 8))
+// }
+
+// function draw() {
+//     noStroke()
+//     const time =   setInterval(()=>{
+//      fill(random(100, 255), random(200), random(50, 150), 100);
+//         circle(random(600), random(700), 30)
+//         clearInterval(time)
+//     }, 100)
+
+// }
 function draw() {
-    background(0);
-    noStroke();
+    noStroke()
     fill(255)
-    rect(circleX, 200, 100, 200)
-    circleX = circleX + 10
+    circle(mouseX, mouseY, 15)
+}
+function mousePressed() {
+    background(0)
 }
